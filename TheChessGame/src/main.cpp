e185270150afae0915ca4b160de8dc253b3a7e49
Sarper AVCI <50243344+sarperavci@users.noreply.github.com>
2024-12-07 23:38:08 +0300

    Playable Terminal Chess Demo

--- TheChessGame/src/main.cpp
+++ TheChessGame/src/main.cpp
@@ -10,5 +10,20 @@ int main()
     // Initialize the game board
     game_board->initiate_board();
     game_board->print_board();
+    
+    while (1)
+    {
+        // Get user input for the move
+       int position, destination;
+       std::cout << "Enter the position and destination: ";
+       std::cin >> position >> destination;
+
+        // Move the piece
+        game_board->move_piece(position, destination);
+        game_board->print_board();
+       
+    }
+
+
     return 0;
 }
