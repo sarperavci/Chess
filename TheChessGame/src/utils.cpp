c6e9c02bad215e319ec148bf804d2662dc6d1b5e
Sarper AVCI <50243344+sarperavci@users.noreply.github.com>
2024-12-07 23:34:43 +0300

    Solving Logic Error

--- TheChessGame/src/utils.cpp
+++ TheChessGame/src/utils.cpp
@@ -1,13 +1,15 @@
 #include <utility>
 
 // Function to convert an integer to 2D array indexes
-std::pair<int, int> intTo2DIndexes(int index) {
-    int row = 7 - (index / 8);
+std::pair<int, int> intTo2DIndexes(int index)
+{
+    int row = index / 8;
     int col = index % 8;
-    return std::make_pair(row, col);
+    return {row, col};
 }
 
 // Function to convert 2D array indexes to an integer
-int indexes2DToInt(int row, int col) {
-    return (7 - row) * 8 + col;
+int indexes2DToInt(int row, int col)
+{
+    return row * 8 + col;
 }
