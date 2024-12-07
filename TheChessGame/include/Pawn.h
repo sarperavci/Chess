be6b08a127ebb0ec80933c483565fabcea855fb9
Sarper AVCI <50243344+sarperavci@users.noreply.github.com>
2024-12-07 13:26:07 +0300

    Add Pawn.h and Pawn.cpp

---
+++ TheChessGame/include/Pawn.h
@@ -0,0 +1,21 @@
+#ifndef PAWN_H
+#define PAWN_H
+
+#include "Piece.h"
+
+class Pawn : public Piece
+{
+private:
+    bool has_moved = false;
+
+public:
+    Pawn(Color color, int position);
+    int get_position() override;
+    Color get_color() override;
+    bool is_valid_move(int destination) override;
+    void move(int destination) override;
+    PieceType get_piece_type() override;
+    std::vector<int> get_valid_moves() override;
+};
+
+#endif
---
+++ TheChessGame/src/Pawn.cpp
@@ -0,0 +1,70 @@
+#include "Pawn.h"
+
+int Pawn::get_position()
+{
+    return position;
+}
+
+Color Pawn::get_color()
+{
+    return color;
+}
+
+PieceType Pawn::get_piece_type()
+{
+    return piece_type;
+}
+
+bool Pawn::is_valid_move(int destination)
+{
+    std::vector<int> valid_moves = get_valid_moves();
+    for (int i = 0; i < valid_moves.size(); i++)
+    {
+        if (destination == valid_moves[i])
+        {
+            return true;
+        }
+    }
+}
+
+void Pawn::move(int destination)
+{
+    if (!has_moved)
+        has_moved = true;
+
+    position = destination;
+}
+
+std::vector<int> Pawn::get_valid_moves()
+{
+    std::vector<int> valid_moves;
+
+    if (color == Color::WHITE)
+    {
+        // Move one square forward
+        if (position + 8 <= 63)
+        {
+            valid_moves.push_back(position + 8);
+        }
+        // Move two squares forward from initial position
+        if (!has_moved && position + 16 <= 63)
+        {
+            valid_moves.push_back(position + 16);
+        }
+    }
+    else // The pawn is black
+    {
+        // Move one square forward
+        if (position - 8 >= 0)
+        {
+            valid_moves.push_back(position - 8);
+        }
+        // Move two squares forward from initial position
+        if (!has_moved && position - 16 >= 0)
+        {
+            valid_moves.push_back(position - 16);
+        }
+    }
+
+    return valid_moves;
+}
